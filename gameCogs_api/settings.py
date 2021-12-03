a81dd0f218eec6b895f808483d84811169cabf6a
Nick Eikenberg <nickeikenberg95@gmail.com>
2021-12-02 23:11:28 -0500

    Created super user and Game model

--- gameCogs_api/settings.py
+++ gameCogs_api/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'gamecogs_api',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'games_api',
 ]
 
 MIDDLEWARE = [
